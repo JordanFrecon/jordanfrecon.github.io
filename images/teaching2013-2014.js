b5423ddf51a89204203b6f0bbf2827f39fdf7cc1
Jordan Frecon <jordan.frecon@gmail.com>
2020-09-07 17:14:04 +0200

    Update teaching2013-2014.js

--- images/teaching2013-2014.js
+++ images/teaching2013-2014.js
@@ -3,7 +3,7 @@ var ctx = document.getElementById('myChart');
 var hours = [18, 20];
 var courses = ['Agregation de physique', 'Science of matter'];
 
-var teaching2014t = new Chart(ctx, {
+var teaching2014 = new Chart(ctx, {
  type: 'polarArea',
  data: {
     labels: courses,
