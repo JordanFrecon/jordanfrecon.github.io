0a1aa8e3f357aa9345f3960c5e9793d795b903c7
Jordan Frecon <jordan.frecon@gmail.com>
2020-09-07 19:33:32 +0200

    two canvas

--- images/teaching.js
+++ images/teaching.js
@@ -10,4 +10,16 @@ window.onload = function() {
       }]
     }
   })
+
+  var ctx2 = document.getElementById("myChart2");
+  var lineChart2 = new Chart(ctx2, {
+    type: 'line',
+    data: {
+      labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"],
+      datasets: [{
+        label: "2015",
+        data: [10, 8, 6, 5, 12, 8, 16, 17, 6, 7, 6, 10]
+      }]
+    }
+  })
 }
