9093f58530a2017f3d77247a38be0ca251505428
Jordan Frecon <jordan.frecon@gmail.com>
2020-09-08 10:37:24 +0200

    added teaching year

--- images/teaching.js
+++ images/teaching.js
@@ -1,6 +1,6 @@
 window.onload = function() {
-  var ctx = document.getElementById("myChart");
-  var lineChart = new Chart(ctx, {
+  var ctx = document.getElementById("teaching2014");
+  var chart = new Chart(ctx, {
     type: 'polarArea',
     data: {
       labels: ["Electromagnetism (L1)","Agregation de Physique"],
@@ -18,8 +18,8 @@ window.onload = function() {
       }
     }
   });
-  var ctx2 = document.getElementById("myChart2");
-  var lineChart2 = new Chart(ctx2, {
+  var ctx2 = document.getElementById("teaching2015");
+  var chart2 = new Chart(ctx2, {
     type: 'polarArea',
     data: {
       labels: ["Electromagnetism (L1)", "Optics (L2)", "Complex systems (M2)", "Applied Statistics (L3)"],
@@ -37,8 +37,8 @@ window.onload = function() {
       }
     }
   });
-  var ctx3 = document.getElementById("myChart3");
-  var lineChart3 = new Chart(ctx3, {
+  var ctx3 = document.getElementById("teaching2016");
+  var chart3 = new Chart(ctx3, {
     type: 'polarArea',
     data: {
       labels: ["Electromagnetism (L1)", "Optics (L2)", "Python (L3)", "Signal processing (L3)"],
