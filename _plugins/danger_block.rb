37f692b3eecea28671112cd208e697d42c16ddff
Jordan Frecon <jordan.frecon@gmail.com>
2020-09-10 18:23:15 +0200

    Create danger_block.rb

---
+++ _plugins/danger_block.rb
@@ -0,0 +1,21 @@
+class GlitchWorks::DangerBlock < GlitchWorks::Block
+
+  def bind_params (params)
+    @add_break = params[:add_break]
+  end
+
+  def internal_render
+    <<~DANGER
+    #{"<div>&nbsp;</div>" if @add_break}
+    <div class='error_explanation'>
+      <div class='error_explanation_content'>
+        <p>
+          #{markdown_converter.convert(@text)}
+        </p>
+      </div>
+    </div>
+    DANGER
+  end
+end
+
+Liquid::Template.register_tag('danger', GlitchWorks::DangerBlock)
