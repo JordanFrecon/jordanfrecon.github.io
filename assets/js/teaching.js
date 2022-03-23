256047eba85e1408e1d04916e91e579e26e07448
Jordan Frecon <jordan.frecon@gmail.com>
2022-03-23 09:15:56 +0100

    Update teaching.js

--- assets/js/teaching.js
+++ assets/js/teaching.js
@@ -41,11 +41,11 @@ window.onload = function() {
   var chart3 = new Chart(ctx3, {
     type: 'doughnut',
     data: {
-      labels: ["Electromagnetism (L1)", "Optics (L2)", "Python (L3)", "Signal processing (L3)"],
+      labels: ["Electromagnetism (L1)", "Optics (L2)", "Experimental Physics (L3)", "Python (L3)", "Signal processing (L3)"],
       datasets: [{
         label: "2015-2016",
-        backgroundColor: ["#e76f51", "#f4a261","#40916c","#52b788"],
-        data: [20, 39.5, 20, 8]
+        backgroundColor: ["#e76f51", "#f4a261"," #27ae60", "#40916c","#52b788"],
+        data: [20, 26, 8, 20, 20]
       }]
     },
     options: {
