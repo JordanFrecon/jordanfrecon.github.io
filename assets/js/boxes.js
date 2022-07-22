ef3981045c73ed5aab82a466fac3aa00baa2f2fb
Jordan Frecon <jordan.frecon@gmail.com>
2022-07-22 15:20:36 +0200

    refactor js

--- assets/js/boxes.js
+++ assets/js/boxes.js
@@ -4,3 +4,12 @@ function toggleIcon(id) {
   x.classList.toggle("fa-angle-double-down");
   console.log(x.classList)
 };
+
+function ShowAndHide(id) {
+    var x = document.getElementById(id);
+    if (x.style.display == 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+};
