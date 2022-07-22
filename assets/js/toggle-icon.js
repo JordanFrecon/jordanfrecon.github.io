6412a7098f3a7182e82f05773a2a5f07ac66ebca
Jordan Frecon <jordan.frecon@gmail.com>
2022-07-22 16:03:33 +0200

    refactor js scripts

---
+++ assets/js/toggle-icon.js
@@ -0,0 +1,9 @@
+function toggleIcon(id) {
+  var x = document.getElementById(id);
+  x.classList.toggle("fa-angle-double-up");
+  x.classList.toggle("fa-angle-double-down");
+  console.log(x.classList);
+}
+
+
+
