6f487af4de9758f7c09eac91904c4f4932c22352
Jordan Frecon <jordan.frecon@gmail.com>
2022-07-27 13:43:16 +0200

    add transparency

---
+++ assets/js/transparency.js
@@ -0,0 +1,5 @@
+import colorLib from '@kurkle/color';
+function transparentize(value, opacity) {
+  var alpha = opacity === undefined ? 0.5 : 1 - opacity;
+  return colorLib(value).alpha(alpha).rgbString();
+}
