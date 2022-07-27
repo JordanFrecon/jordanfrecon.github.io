c09671016d60448783cea9d4f891aafa0a8b14bb
Jordan Frecon <jordan.frecon@gmail.com>
2022-07-27 14:09:26 +0200

    Delete transparency.js

--- assets/js/transparency.js
+++
@@ -1,5 +0,0 @@
-import colorLib from '@kurkle/color';
-function transparentize(value, opacity) {
-  var alpha = opacity === undefined ? 0.5 : 1 - opacity;
-  return colorLib(value).alpha(alpha).rgbString();
-}
