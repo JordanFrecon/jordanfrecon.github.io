dc54c064ae8509c8f4831c89a14c29a42239a202
Jordan Frecon <jordan.frecon.deloire@univ-st-etienne.fr>
2025-12-16 09:43:34 +0100

    added preli ml chap 6

---
+++ courses/machine-learning/kernels/assets/positive-DZzW_Qfo.js
@@ -0,0 +1,1 @@
+const s="/courses/machine-learning/kernels/ml_chap6/symmetry.png",e="/courses/machine-learning/kernels/ml_chap6/positive.png";export{s as _,e as a};
---
+++ courses/machine-learning/kernels/assets/vector-CSnvAC5Y.js
@@ -0,0 +1,1 @@
+const e="/courses/machine-learning/kernels/ml_chap6/vector.png";export{e as _};
