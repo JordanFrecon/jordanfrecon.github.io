bd9cc9f6825e1297bc49e466783eb14b11039b17
Jordan Frecon <jordan.frecon.deloire@univ-st-etienne.fr>
2026-01-14 14:37:09 +0100

    add course ml learning theory

---
+++ courses/machine-learning/learning-theory/assets/hypothesis-space-pDxCaqQ9.js
@@ -0,0 +1,1 @@
+const e="/courses/machine-learning/learning-theory/ml_chap2/hypothesis-space.png";export{e as _};
---
+++ courses/machine-learning/learning-theory/assets/icon_hypothesis-space-distinct-Y8fpHa1E.js
@@ -0,0 +1,1 @@
+const n="/courses/machine-learning/learning-theory/ml_chap2/icon_hypothesis-space-distinct.png";export{n as _};
